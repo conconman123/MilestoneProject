b3bf590e130135dba13ec82ee150e5038b01377e
Conor Panuccio <conorpanu@hotmail.com>
2023-06-21 15:20:33 -0400

    added base game functions

--- index.js
+++ index.js
@@ -1,1 +1,429 @@
+let card = [
+    {
+        name: '2S',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/2.5.png',
+        value: 2
+    },
+    {
+        name: '2H',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/2.6.png',
+        value: 2
+    },
+    {
+        name: '2C',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/2.7.png',
+        value: 2
+    },
+    {
+        name: '2D',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/2.8.png',
+        value: 2
+    },
+    {
+        name: '3S',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/3.5.png',
+        value: 3
+    },
+    {
+        name: '3H',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/3.6.png',
+        value: 3
+    },
+    {
+        name: '3C',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/3.7.png',
+        value: 3
+    },
+    {
+        name: '3D',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/3.8.png',
+        value: 3
+    },
+    {
+        name: '4S',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/4.5.png',
+        value: 4
+    },
+    {
+        name: '4H',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/4.6.png',
+        value: 4
+    },
+    {
+        name: '4C',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/4.7.png',
+        value: 4
+    },
+    {
+        name: '4D',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/4.8.png',
+        value: 4
+    },
+    {
+        name: '5S',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/5.5.png',
+        value: 5
+    },
+    {
+        name: '5H',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/5.6.png',
+        value: 5
+    },
+    {
+        name: '5C',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/5.7.png',
+        value: 5
+    },
+    {
+        name: '5D',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/5.8.png',
+        value: 5
+    },
+    {
+        name: '6S',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/6.5.png',
+        value: 6
+    },
+    {
+        name: '6H',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/6.6.png',
+        value: 6
+    },
+    {
+        name: '6C',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/6.7.png',
+        value: 6
+    },
+    {
+        name: '6D',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/6.8.png',
+        value: 6
+    },
+    {
+        name: '7S',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/7.5.png',
+        value: 7
+    },
+    {
+        name: '7H',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/7.6.png',
+        value: 7
+    },
+    {
+        name: '7C',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/7.7.png',
+        value: 7
+    },
+    {
+        name: '7D',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/7.8.png',
+        value: 7
+    },
+    {
+        name: '8S',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/8.5.png',
+        value: 8
+    },
+    {
+        name: '8H',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/8.6.png',
+        value: 8
+    },
+    {
+        name: '8C',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/8.7.png',
+        value: 8
+    },
+    {
+        name: '8D',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/8.8.png',
+        value: 8
+    },
+    {
+        name: '9S',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/9.5.png',
+        value: 9
+    },
+    {
+        name: '9H',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/9.6.png',
+        value: 9
+    },
+    {
+        name: '9C',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/9.7.png',
+        value: 9
+    },
+    {
+        name: '9D',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/9.8.png',
+        value: 9
+    },
+    {
+        name: '10S',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/10.5.png',
+        value: 10
+    },
+    {
+        name: '10H',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/10.6.png',
+        value: 10
+    },
+    {
+        name: '10C',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/10.7.png',
+        value: 10
+    },
+    {
+        name: '10D',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/10.8.png',
+        value: 10
+    },
+    {
+        name: 'JS',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/J5.png',
+        value: 10
+    },
+    {
+        name: 'JH',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/J6.png',
+        value: 10
+    },
+    {
+        name: 'JC',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/J7.png',
+        value: 10
+    },
+    {
+        name: 'JD',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/J8.png',
+        value: 10
+    },
+    {
+        name: 'QS',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/Q5.png',
+        value: 10
+    },
+    {
+        name: 'QH',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/Q6.png',
+        value: 10
+    },
+    {
+        name: 'QC',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/Q7.png',
+        value: 10
+    },
+    {
+        name: 'QD',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/Q8.png',
+        value: 10
+    },
+    {
+        name: 'KS',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/K5.png',
+        value: 10
+    },
+    {
+        name: 'KH',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/K6.png',
+        value: 10
+    },
+    {
+        name: 'KC',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/K7.png',
+        value: 10
+    },
+    {
+        name: 'KD',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/K8.png',
+        value: 10
+    },
+    {
+        name: 'AS',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/A.5.png',
+        value: 1
+    },
+    {
+        name: 'AH',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/A.6.png',
+        value: 1
+    },
+    {
+        name: 'AC',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/A.7.png',
+        value: 1
+    },
+    {
+        name: 'AD',
+        cardBack: './deck-of-cards/Back7.png',
+        cardFace: './deck-of-cards/A.8.png',
+        value: 1
+    }
+]
 
+//game varianbles
+let playing = true
+let dealersCards = []
+let playersCards = []
+playerValue = 0
+dealerValue = 0
+
+//make dom elements
+let hitButton = document.getElementById('hit')
+let dealButton = document.getElementById('deal')
+let standButton = document.getElementById('stand')
+let resultDiv = document.getElementById('result')
+let playerHandDiv = document.getElementById('player-hand')
+let dealerHandDiv = document.getElementById('dealer-hand')
+
+hitButton.addEventListener('click', hit)
+dealButton.addEventListener('click', deal)
+standButton.addEventListener('click', stand)
+
+function createDeck(card){
+    let deck = [card.length]
+    for(let i = 0; i < card.length; i++){
+        deck[i] = card[i]
+    }
+    return deck
+}
+
+function drawCard(){
+    return deck.shift()
+}
+
+function shuffle(deck) {
+    for(let i = 0; i < deck.length; i++){
+        let swapID = Math.trunc(Math.random() * deck.length)
+        let temp = deck[swapID]
+        deck[swapID] = deck[i]
+        deck[i] = temp
+    }
+}
+
+function checkValue(cards){
+    let total = 0
+    let ace =  false
+    for(let i = 0; i < cards.length; i++){
+        total += cards[i].value
+        if(cards[i].value == 1){
+            ace = true
+        }
+    }
+    if(ace && total + 10 <= 21){
+        return total + 10
+    }else{
+        return total
+    }
+}
+
+function checkGame(){
+    if(playing  = false){
+        if(playerValue > 21){
+            resultDiv.textContent = "you busted dealer wins"
+        }else if(dealerValue > 21){
+            resultDiv.textContent = "dealer busted you win"
+        }else if(playerValue === dealerValue){
+            resultDiv.textContent = "its a tie"
+        }else if(playerValue > dealerValue){
+            resultDiv.textContent = "you win"
+        }else if(dealerValue > playerValue){
+            resultDiv.textContent = "dealer wins"
+        }
+        dealButton.disabled = false
+        hitButton.disabled = true
+        standButton.disabled = true
+    }else{
+        resultDiv.textContent = ''
+    }
+}
+
+function deal(){
+    console.log('workinh')
+    deck = createDeck()
+    shuffle(deck)
+    for(let i = 0; i < 2; i++){
+        playersCards.push(drawCard())
+        dealersCards.pysh(drawCard())
+    }
+    playerValue = checkValue(playersCards)
+    dealerValue = checkValue(dealersCards)
+    playing = true
+    checkGame()
+    dealButton.disabled = true
+    hitButton.disabled = false
+    standButton.disabled = false
+}
+
+function hit(){
+    playersCards.push(drawCard())
+    playerValue = checkValue(playersCards)
+    if(playerValue > 21){
+        playing = false
+    }
+    checkGame()
+}
+
+function stand(){
+    while(dealerValue < 17 && (playing = true)){
+        dealersCards.push(drawCard())
+        dealerValue = checkValue(dealersCards())
+        if(dealerValue > 21){
+            playing =  false
+        }
+    }
+    playing = false
+    checkGame()
+}
